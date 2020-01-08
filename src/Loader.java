b10201f6c42984889ff80475833a9e3b09a35980
AlexRzh <a.rzhevtsev@netrika.ru>
2020-01-08 18:57:40 +0300

    Initial commit

---
+++ src/Loader.java
@@ -0,0 +1,26 @@
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Terminal;
+import com.skillbox.airport.Flight;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class Loader {
+    public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+        Terminal terminal = airport.getTerminals().get(0);
+        List<Flight> flights = terminal.getFlights();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR, +2);
+        Date plus2hours = calendar.getTime();
+        Date nowTime = new Date();
+
+        flights.stream()
+                .filter(d -> d.getType().compareTo(Flight.Type.DEPARTURE) > -1)
+                .map(Flight::getDate)
+                .filter(s -> s.before(plus2hours) && s.after(nowTime))
+                .forEach(System.out::println);
+    }
+}
